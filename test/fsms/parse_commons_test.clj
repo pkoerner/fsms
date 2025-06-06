4e6c31d43711bde5da05b90ce27968f4358c61b6
Philipp Koerner <p.koerner@uni-duesseldorf.de>
2025-06-06 19:39:18 +0200

    add tests, one failing

---
+++ test/fsms/parse_commons_test.clj
@@ -0,0 +1,21 @@
+(ns fsms.parse-commons-test
+  (:use [clojure.test]
+        [fsms.parse-commons]))
+
+(deftest final-invalid-test
+  (testing "invalid accepting state declaration is recognized"
+    (is (thrown? AssertionError (parse-final-states "final z0 ### z1")))
+    (is (thrown-with-msg? AssertionError #"No accepting states"
+                          (parse-final-states "final")))
+    (is (thrown-with-msg? AssertionError #"No accepting states"
+                          (parse-final-states "final    ")))
+    (is (thrown-with-msg? AssertionError #"No accepting states"
+                          (parse-final-states "finalz    ")))))
+
+(deftest final-vaid-test
+  (testing "valid accepting state declaration returns proper values"
+    (are [x y] (= (parse-final-states x) y)
+         "final z0" {:final-states #{"z0"}}
+         "final z0 z1" {:final-states #{"z0" "z1"}}
+         "final z_0 z_1" {:final-states #{"z_0" "z_1"}}
+         "final z_0z2 z_1" {:final-states #{"z_0z2" "z_1"}})))
